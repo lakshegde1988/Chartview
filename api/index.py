bc1a1f8d816f47e5e5166462560fbcd7a5428795
lakshegde1988 <lakshhegde1988@gmail.com>
2024-10-31 22:39:10 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,12 +1,8 @@
 from flask import Flask, render_template, jsonify, request
 import yfinance as yf
 import pandas as pd
-import logging
 
 app = Flask(__name__)
-
-# Configure logging
-logging.basicConfig(level=logging.DEBUG)
 
 @app.route('/')
 def index():
@@ -17,7 +13,6 @@ def index():
 @app.route('/get_ohlc')
 def get_ohlc():
     symbol = request.args.get('symbol')
-    logging.debug(f"Fetching OHLC data for symbol: {symbol}")  # Log the requested symbol
     try:
         # Fetch OHLC data
         data = yf.download(symbol + '.NS', period='3mo', interval='1d')
@@ -32,11 +27,8 @@ def get_ohlc():
         
         # Convert to a list of dictionaries
         result = data.to_dict(orient='records')  # Use orient='records' for a list of dictionaries
-        logging.debug(f"Data fetched for {symbol}: {result}")  # Log the fetched data
         return jsonify(result)
     except Exception as e:
-        # Log the error for debugging
-        logging.error(f"Error fetching data for {symbol}: {e}")
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
